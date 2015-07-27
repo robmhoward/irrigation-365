a0948e2d23b5f8ae4c636a6f09038fec072aec70
Rob Howard <rhoward@Robs-MacBook-Pro.local>
2015-07-27 09:46:16 -0700

    Partial updates to calendar service interaction

--- getServiceData.js
+++ getServiceData.js
@@ -2,14 +2,26 @@ var https = require('https');
 var request = require('request');
 
 module.exports = {
-	getHistoricalData: getHistoricalData,
-	getPredictionData: getPredictionData,
-	sendPredictionEmail: sendPredictionEmail,
+	getWateringAppointments: getWateringAppointments,
 	sendSummaryEmail: sendSummaryEmail
 }
 
-var msHealthHostName = "apibeta.microsofthealth.net";
 var calendarHostName = "outlook.office365.com";
+
+function getWateringAppointments(startDate, endDate, callback) {
+	request({
+	    url: "https://" + calendarHostName + "/api/v1.0/me/calendarview?startDateTime=" + startDate.toISOString() + "&endDateTime=" + endDate.toISOString() + "&$select=Subject,Start,End,Location,Id&$filter=Subject eq 'Water'&$orderBy=Start,End",
+		method: "GET",
+		headers: {
+			'Authorization': 'Bearer ' + user.aadTokens.accessToken
+		},
+		port: 443,
+	    json: true
+	}, function (error, response, body){
+		if (error) console.log(error);
+		callback(error, response);
+	});
+}
 
 function createFriendlyTimeString(dateString) {
 	var date = new Date(dateString);
@@ -193,182 +205,6 @@ function getPredictionData(startDate, endDate, user, callback) {
 			callback(null, calendarEvents);
 		});
 	});
-}
-
-
-function getPredictedHeartRates(calendarEvents, callback) {
-	var completedCalls = 0;
-	
-	for (var i = 0; i < calendarEvents.length; i++) {
-		getPredictedHeartRate(calendarEvents[i], function(err, result) {
-			completedCalls++;
-			if (completedCalls >= calendarEvents.length) {
-				console.log("all calls completed");
-				callback();
-			}
-		});
-	}
-}
-
-function getPredictedHeartRate(event, callback) {
-	var requestBody = 
-	{
-		"Id":"score1",
-		"Instance":
-			{
-				"FeatureVector":
-					{
-						"userID":"0",
-						"meetingSubject": event.Subject,
-						"meetingAttendees":"0",
-						"meetingBody": event.Body.Content,
-						"meetingLocation": event.Location.DisplayName,
-						"meetingOrganizer": event.Organizer.EmailAddress.Address,
-						"meetingStart": event.Start,
-						"meetingEnd": event.End,
-						"meetingImportance": event.Importance,
-						"meetingType": event.Type,
-						"userAverageHeartRate":"0"
-					},
-				"GlobalParameters":{}
-			}
-	};
-
-	request({
-	    url: "https://ussouthcentral.services.azureml.net/workspaces/b5049852f1c24d9ebbfcd48fdbacce2a/services/01928aeaa447471aa9ccfe5af47542d3/score",
-		method: "POST",
-		headers: {
-			'Authorization': 'Bearer 8yCAqdgEjqcAIKMwWG66TZPQ2DEGVX0OS9O00+4ozmpiFN/WfmBPHWqtwRI00GmOexAhT800xmnVyLfQxWTlmA=='
-		},
-		port: 443,
-	    json: true,
-	    body: requestBody
-		}, function (error, response, body){
-			if (error) console.log(error);
-			event.PredictedHeartRate = body[3];
-			callback(null, body[3]);
-		});	
-}
-
-function getCalendarData(startDate, endDate, user, callback) {
-	var calendarResponseData = "";
-	var calendarRequest = https.request({
-		hostname: calendarHostName,
-		port: 443,
-		path: '/api/v1.0/me/calendarview?startDateTime=' + startDate.toISOString() + '&endDateTime=' + endDate.toISOString() + '&$select=Subject,Attendees,Body,BodyPreview,Location,Organizer,Start,StartTimeZone,End,EndTimeZone,Importance,Type,ResponseStatus&$orderBy=Start,End',
-		method: 'GET',
-		headers: {
-			'Accept': 'application/json',
-			'Authorization': 'Bearer ' + user.aadTokens.accessToken
-		}			
-	}, function(calendarResponse) {
-		calendarResponse.on("error", function(error) {
-			console.log(error.message);
-		});
-		calendarResponse.on("data", function(data) {
-			calendarResponseData += data.toString();
-		});
-		calendarResponse.on("end", function() {
-			callback(null, calendarResponseData);
-		});
-	});
-	calendarRequest.end();
-}
-
-
-function getHistoricalData(startDate, endDate, user, callback) {
-	
-	var requestsCompleted = 0;
-
-	if (user && user.msaTokens && user.aadTokens) {
-		var healthResponseData = "";
-		var calendarResponseData = "";
-		
-		var healthRequest = https.request({
-			hostname: msHealthHostName,
-			port: 443,
-			path: '/v1/me/summaries/hourly?startTime=' + startDate.toISOString() + '&endTime=' + endDate.toISOString(),
-			method: 'GET',
-			headers: {
-				'Accept': 'application/json',
-				'Authorization': 'Bearer ' + user.msaTokens.accessToken
-			}
-		}, function(healthResponse) {
-			healthResponse.on("error", function(error) {
-				console.log(error.message);
-			});
-			healthResponse.on("data", function(data) {
-				healthResponseData += data.toString();
-			});
-			healthResponse.on("end", function() {
-				if (healthResponse.statusCode >= 400) {
-					console.log("Error retrieving health info. Code: " + healthResponse.statusCode)
-				}
-				checkForCompletionAndProceed();
-			});
-		});
-		healthRequest.end();
-						
-		var calendarRequest = https.request({
-			hostname: calendarHostName,
-			port: 443,
-			path: '/api/v1.0/me/calendarview?startDateTime=' + startDate.toISOString() + '&endDateTime=' + endDate.toISOString() + '&$select=Subject,Attendees,Body,BodyPreview,Location,Organizer,Start,StartTimeZone,End,EndTimeZone,Importance,Type,ResponseStatus&$orderBy=Start,End&$top=20',
-			method: 'GET',
-			headers: {
-				'Accept': 'application/json',
-				'Authorization': 'Bearer ' + user.aadTokens.accessToken
-			}			
-		}, function(calendarResponse) {
-			calendarResponse.on("error", function(error) {
-				console.log(error.message);
-			});
-			calendarResponse.on("data", function(data) {
-				calendarResponseData += data.toString();
-			});
-			calendarResponse.on("end", function() {
-				if (calendarResponse.statusCode >= 400) {
-					console.log("Error retrieving calendar info. Code: " + calendarResponse.statusCode)
-				}
-				checkForCompletionAndProceed();
-			});
-		});
-		calendarRequest.end();
-		
-		function checkForCompletionAndProceed() {
-			requestsCompleted++;
-			if (requestsCompleted == 2) {
-				callback(null, mergeHealthAndCalendarData(JSON.parse(healthResponseData), JSON.parse(calendarResponseData)));
-			}
-		}
-	} else {
-		callback("no tokens", null);
-	}
-}
-
-function mergeHealthAndCalendarData(healthData, calendarData) {
-	var healthHours = healthData.summaries;
-	var events = calendarData.value;
-	
-	for (var i = 0; i < events.length; i++) {
-		var heartRateTotal = 0;
-		var overlappingHourCount = 0;
-		var stepCount = 0;
-		for (var j = 0; j < healthHours.length; j++) {
-			if (overlap(healthHours[j].startTime, healthHours[j].endTime, events[i].Start, events[i].End) && healthHours[j].heartRateSummary.averageHeartRate) {
-				overlappingHourCount++;
-				heartRateTotal += healthHours[j].heartRateSummary.averageHeartRate;
-				stepCount += healthHours[j].stepsTaken;
-			}
-		}
-		events[i].StepsTaken = stepCount;
-		if (overlappingHourCount > 0) {
-			events[i].AverageHeartRate = heartRateTotal / overlappingHourCount;
-		} else {
-			events[i].AverageHeartRate = 0;
-		}
-	}
-	
-	return events;
 }
 
 function overlap(startDateOne, endDateOne, startDateTwo, endDateTwo) {
